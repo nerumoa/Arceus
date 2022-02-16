cbe8f59794b3ed337e5db088176c39ae73a4ddcf
nerumoa019 <50538677+nerumoa019@users.noreply.github.com>
2022-02-16 22:46:31 +0900

    カメラ回り修正

--- Assets/Scripts/Nerumoa/ChangeCamera.cs
+++ Assets/Scripts/Nerumoa/ChangeCamera.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 public class ChangeCamera : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera vCamera = default;
+    [SerializeField] Collider2D confiner1 = default;
+    [SerializeField] Collider2D confiner2 = default;
+
+    [SerializeField] bool inversion = false;
+
+    CinemachineConfiner cc;
+
+    private void Start()
+    {
+        cc = vCamera.gameObject.GetComponent<CinemachineConfiner>();
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.tag == "Player") {
-            var current = Camera.main.GetComponent<CinemachineBrain>().ActiveVirtualCamera as CinemachineVirtualCamera;
-            current.Priority = 0;
-            vCamera.Priority = 100;
+        if (col.CompareTag("Player")) {
+            if (inversion) {
+                confiner1.gameObject.SetActive(true);
+                cc.m_BoundingShape2D = confiner1;
+                confiner2.gameObject.SetActive(false);
+            } else {
+                confiner2.gameObject.SetActive(true);
+                cc.m_BoundingShape2D = confiner2;
+                confiner1.gameObject.SetActive(false);
+            }
         }
     }
 }
